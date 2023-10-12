7702c2a6254e05fdcbd8faa71984d6db52e4a09f
FinnCl4rk <phinphan11@gmail.com>
2023-10-12 12:54:34 -0700

    To be continued....

--- src/ForLoops.java
+++ src/ForLoops.java
@@ -4,6 +4,14 @@ public class ForLoops {
     private final static Scanner s = new Scanner(System.in);
 
     public static void forSum() {
+        int sum = 0;
+        System.out.println("Number?");
+        int n = s.nextInt();
+        for (int step = 1; step <= n; n = step + 1) {
+            int add = sum;
+            break;
+        }
+        System.out.println("The sum is: " + add);
     }
 
     public static void rangeSum() {
