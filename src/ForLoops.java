12868f6a89daf5c48da4ba4b7d4e64353db1aef7
Justin hart <justinhart201@outlook.com>
2023-10-12 12:03:30 -0700

    lab07

--- src/ForLoops.java
+++ src/ForLoops.java
@@ -1,20 +1,73 @@
 import java.util.Scanner;
 
+
+// ForLoops class
 public class ForLoops {
-    private final static Scanner s = new Scanner(System.in);
 
+    // forSum method
     public static void forSum() {
+        // create a Scanner object to read user input
+        Scanner sc = new Scanner(System.in);
+        // prompt the user to enter a number
+        System.out.print("Number? ");
+        // read the number as an integer
+        int n = sc.nextInt();
+        // initialize a variable to store the sum
+        int sum = 0;
+        // use a for loop to iterate from 1 to n
+        for (int i = 1; i <= n; i++) {
+            // add i to the sum
+            sum += i;
+        }
+        // print the sum
+        System.out.println("The sum is " + sum);
     }
 
+    // rangeSum method
     public static void rangeSum() {
+        // create a Scanner object to read user input
+        Scanner sc = new Scanner(System.in);
+        // prompt the user to enter two numbers
+        System.out.print("Number? ");
+        int a = sc.nextInt();
+        System.out.print("Number? ");
+        int b = sc.nextInt();
+        // initialize a variable to store the sum
+        int sum = 0;
+        // use Math.min and Math.max to find the smaller and larger number
+        int min = Math.min(a, b);
+        int max = Math.max(a, b);
+        // use a for loop to iterate from min to max
+        for (int i = min; i <= max; i++) {
+            // add i to the sum
+            sum += i;
+        }
+        // print the sum and the range
+        System.out.println("The sum is: " + sum);
+        System.out.println("The sum in the above example is the result of: " + min + " + ... + " + max);
     }
 
+    // factorial method
     public static void factorial() {
+        // create a Scanner object to read user input
+        Scanner sc = new Scanner(System.in);
+        // prompt the user to enter a number
+        System.out.print("Number? ");
+        // read the number as an integer
+        int n = sc.nextInt();
+        // initialize a variable to store the factorial
+        int fact = 1;
+        // use a for loop to iterate from 1 to n
+        for (int i = 1; i <= n; i++) {
+            // multiply i to the factorial
+            fact *= i;
+        }
+        // print the factorial
+        System.out.println(n + "! = " + fact);
     }
 
     public static void main(String[] args) {
         forSum();
-        rangeSum();
-        factorial();
     }
 }
+
--- src/ReWrites.java
+++ src/ReWrites.java
@@ -1,47 +1,66 @@
 import java.util.Scanner;
 
-class ReWrites {
-    private final static Scanner s = new Scanner(System.in);
-
-    public static void avgRewrite() {
-        String l;
-        double sum = 0.0;
-        int count = 0;
-        System.out.println("Ths program will calculate the average of numbers entered. Begin entering and enter \"Q\" to quit.");
-        while(!(l = s.nextLine()).toUpperCase().equals("Q")) {
-            try {
-                sum += Integer.parseInt(l);
-                count++;
-            } catch (NumberFormatException nfe) {}
-        }
-
-        System.out.printf("Average is: %f%n", (sum/count));
-    }
-    public static void sumRewrite() {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Type positive integers to sum. To stop, type 0...");
-        int x = -1;
-        int sum = 0;
-
-        while (true) {
-            try {
-                if(x == 0) {
-                    break;
-                }
-                x = Integer.parseInt(in.nextLine());
-                if (x <= 0) {
-                    continue;
-                }
-                System.out.println("Adding " + x);
-                sum += x;
-            } catch (NumberFormatException nfe) {}
-        }
-
-        System.out.printf("Sum: %d%nGood Bye%n", sum);
-    }
-
+// ReWrites class
+public class ReWrites {
     public static void main(String[] args) {
-        avgRewrite();
-        sumRewrite();
+ sumRewrite();
+    }
+    // sumRewrite method
+    public static void sumRewrite() {
+        // create a Scanner object to read user input
+        Scanner sc = new Scanner(System.in);
+        // initialize variables to store the sum and the number
+        int sum = 0;
+        int num = 0;
+        // use a while loop to read integers until the user enters -1
+        while (num != -1) { // change the condition from true to num != -1
+            // prompt the user to enter an integer
+            System.out.print("Enter an integer (-1 to stop): ");
+            // read the integer as an int
+            num = sc.nextInt();
+            // check if the number is -1
+            if (num == -1) {
+                // remove the break statement
+            } else {
+                // add the number to the sum
+                sum += num;
+            }
+        }
+        // print the sum
+        System.out.println("The sum is " + sum);
+    }
+
+    // avgRewrite method
+    public static void avgRewrite() {
+        // create a Scanner object to read user input
+        Scanner sc = new Scanner(System.in);
+        // initialize variables to store the sum, count and number
+        int sum = 0;
+        int count = 0;
+        int num = 0;
+        // use a for loop instead of a while loop to read integers until the user enters -1
+        for (; num != -1; count++) { // initialize count as 0 and increment it in each iteration
+            // prompt the user to enter an integer
+            System.out.print("Enter an integer (-1 to stop): ");
+            // read the integer as an int
+            num = sc.nextInt();
+            // check if the number is -1
+            if (num == -1) {
+                // break out of the loop
+                break;
+            }
+            // add the number to the sum
+            sum += num;
+        }
+        // check if the count is zero to avoid division by zero error
+        if (count == 0) {
+            // print a message that no numbers were entered
+            System.out.println("No numbers were entered.");
+        } else {
+            // calculate the average as a double by dividing sum by count
+            double avg = (double) sum / count;
+            // print the average with two decimal places using printf method
+            System.out.printf("The average is %.2f\n", avg);
+        }
     }
 }
