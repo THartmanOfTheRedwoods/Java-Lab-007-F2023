e4d84d4e29eaac670fbda1230a085b0f4b9205db
Atticus Blanco <atticusbrujx@gmail.com>
2023-10-18 22:13:54 -0700

    Part 1 ---NONOPERATIONAL---
    >>>MODIFICATIONS
    --WhileLoops.java
      -BUILT eoCount method
    
    >>>DEFICITS
    --WhileLoops.java
      -ENCAPSULATE ArrayList methods in a class file
      -BUILD mean method
      -CHANGE even and odd within eoCount method to ArrayList methods.
    
    >>>BUGS
    --WhileLoops.java
      -NONOP mean method
    
    >>>THROWS
    --C:\Users\Gnomatic\IdeaProjects\Java-Lab-007\src\WhileLoops.java
      -Warning:(24, 24) Method 'mean()' is never used
      -Warning:(26, 28) Contents of collection 'integerList' are updated, but never queried

--- src/WhileLoops.java
+++ src/WhileLoops.java
@@ -1,16 +1,44 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class WhileLoops {
     private final static Scanner s = new Scanner(System.in);
+    private static int loopCounter = 0;
 
-    public static void eoCount() {
+    public static void eoCount(int eoSum) {
+        while (eoSum > 0) {
+            if (eoSum % 2 == 0) {
+                loopCounter++;
+                System.out.println(eoSum);  //change to an ArrayList
+                eoSum -= 2;
+            } else {
+                loopCounter++;
+                System.out.println(eoSum);  //change to an ArrayList
+                eoSum -= 2;
+            }
+        }
+        System.out.println("Number of Loops: " + loopCounter);
+        loopCounter = 0;
     }
 
     public static void mean() {
+        System.out.println("Enter values to calculate a mean.\nEnter \"Q\" to begin calculation.");
+        ArrayList<Integer> integerList = new ArrayList<>();
+
+        String userInput = s.nextLine();
+        while (!(userInput.equalsIgnoreCase("Q"))) {
+            integerList.add(Integer.parseInt(userInput));
+            loopCounter++;
+        }
+        
     }
 
     public static void main(String[] args) {
-        eoCount();
-        mean();
+        System.out.println("Enter an integer value");
+        int userInt = Integer.parseInt(s.nextLine());
+        
+        eoCount(userInt);
+
+        //mean();
     }
 }
