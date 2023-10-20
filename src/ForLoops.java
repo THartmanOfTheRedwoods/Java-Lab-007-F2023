a5ab7032279dafb1874dfebfd8709116bcab74dd
Atticus Blanco <atticusbrujx@gmail.com>
2023-10-19 20:28:50 -0700

    Part 2 >>>NONOPERATIONAL --ForLoops.java --ReWrites.java
    >>>OPERATIONAL
    --
    
    >>>MODIFICATIONS
    --ForLoops.java
      -BUILT forSum method
    
    >>>DEFICITS
    --ForLoops.java
      -INCOMPLETE
    --ReWrites.java
      -INCOMPLETE
    
    >>>BUGS
    --WhileLoops.java
      -ENCAPSULATE ArrayList out of mean method
    
    >>>THROWS
    --

--- src/ForLoops.java
+++ src/ForLoops.java
@@ -4,6 +4,15 @@ public class ForLoops {
     private final static Scanner s = new Scanner(System.in);
 
     public static void forSum() {
+        System.out.print("Number? ");
+        int userInt = Integer.parseInt(s.nextLine());
+        int userSum = 0;
+
+        for (int i = 0; i <= userInt; i++) {
+            userSum += i;
+        }
+
+        System.out.println("The sum is " + userSum);
     }
 
     public static void rangeSum() {
