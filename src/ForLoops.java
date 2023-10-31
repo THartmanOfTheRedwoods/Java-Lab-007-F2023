4e047293f6e0071a38f6247802d5c3ba6bdd57d4
LiviMcK <137836813+LiviMcK@users.noreply.github.com>
2023-10-31 12:02:22 -0700

    Finished lb 007

--- src/ForLoops.java
+++ src/ForLoops.java
@@ -3,18 +3,56 @@ import java.util.Scanner;
 public class ForLoops {
     private final static Scanner s = new Scanner(System.in);
 
-    public static void forSum() {
+    public static void forSum() { // method forSum
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Number? "); // asks user to input number
+        int n = scanner.nextInt();
+
+        int sum = 0; // sets sum to 0
+        for (int i = 1; i <= n; i++) { // sets i to 1, i is less than or equal to 1, i will be incremented by 1 after each iteration
+            sum += i; // adds i to sum
+        }
+
+        System.out.println("The sum is " + sum); // prints The sum is: with the calculated sum
     }
 
     public static void rangeSum() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Number? "); // asks user to enter number
+        int num1 = scanner.nextInt(); // reads the input and stores in num1
+        System.out.print("Number? "); // asks user to enter number
+        int num2 = scanner.nextInt(); // reads the input and stores in num2
+
+        int min = Math.min(num1, num2); // used to determine the minimum value
+        int max = Math.max(num1, num2); // used to determine the maximum value
+
+        int sum = 0; // initializes sum to 0
+        for (int i = min; i <= max; i++) { // initializes integer variable to i, sets a condition for loop to continue and specifies that i should be incremented by 1 after each iteration
+            sum += i; // adds value i to sum
+        }
+
+        System.out.println("The sum is: " + sum); // prints The sum is: with the calculated sum
     }
 
     public static void factorial() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Number: "); // asks for user to enter a number
+        int num = scanner.nextInt(); // reads input and stores in num
+
+        int factorial = 1; // sets factorial to 1
+        for (int i = 1; i <= num; i++) { // initializes i to 1, sets conditions for loop to continue, increments i after each interation
+            factorial *= i; // multiplies factorial by the value i
+        }
+
+        System.out.println("Factorial: " + factorial); // prints Factorial: with the calculated factorial
     }
 
     public static void main(String[] args) {
-        forSum();
-        rangeSum();
-        factorial();
+        forSum(); // calls forSum
+        rangeSum(); // calls rangeSum
+        factorial(); // calls factorial
     }
 }
--- src/WhileLoops.java
+++ src/WhileLoops.java
@@ -1,16 +1,69 @@
 import java.util.Scanner;
 
+/**
+ * @author Olivia McKittrick
+ * @date 10/31/2023
+ */
 public class WhileLoops {
     private final static Scanner s = new Scanner(System.in);
 
     public static void eoCount() {
+        Scanner scanner = new Scanner(System.in); // reads integers from System.in
+
+        int evenCount = 0; // even integers separated from odd
+        int oddCount = 0; // odd
+
+        System.out.println("Enter integers (0 to exit):"); // Prints question for user asking for a variation of numbers to use to calculate how many even and odd #'s
+        int num; // declaring variable num
+
+        while(true) { // while statement that makes loop indefinite.
+            num = scanner.nextInt();
+
+            if (num == 0) { // if the user enters 0, the loop ends
+                break;
+            }
+
+            if (num % 2 == 0) { // verifies whether number is even or odd by checking if it is divisible by 2
+                evenCount++;
+            } else { // if not, it is odd
+                oddCount++;
+            }
+        }
+
+        System.out.println("Even numbers: " + evenCount);
+        System.out.println("Odd numbers: " + oddCount);
     }
 
-    public static void mean() {
+    public static void mean() { // method mean
+        Scanner scanner = new Scanner(System.in);
+
+        int sum = 0; // declares the variable sum as 0
+        int count = 0; // declares the variable count as 0
+
+        System.out.println("Enter integers (0 to calculate mean):"); // asks user to enter numbers to calculate the mean
+        int num;
+
+        while (true) { // while statement which remains true
+            num = scanner.nextInt();
+
+            if (num == 0) { // until broken when user enters 0
+                break;
+            }
+
+            sum += num; // num value stored in sum
+            count++; // keeps track of how many numbers have been entered
+        }
+
+        if (count > 0) { // if the number is greater than 0
+            double mean = (double) sum / count; // calculating the average number of a set of numbers
+            System.out.println("Mean: " + mean); // prints the mean with Mean:
+        } else {
+            System.out.println("No numbers entered.");
+        }
     }
 
     public static void main(String[] args) {
-        eoCount();
-        mean();
+        eoCount(); // calls eoCount
+        mean(); // calls mean
     }
 }
