665d666ece17ebe4caf62852ceea5b76e1abcf5c
fires <mlarose859@mycr.redwoods.edu>
2023-11-04 18:39:27 -0700

    Finished lab 7

--- src/WhileLoops.java
+++ src/WhileLoops.java
@@ -4,9 +4,27 @@ public class WhileLoops {
     private final static Scanner s = new Scanner(System.in);
 
     public static void eoCount() {
+int number = s.nextInt();
+        while (true) {
+            if(number%2 == 0){
+        System.out.println("Even " + number);
+            break;}
+       else{
+            System.out.println("Odd " + number);
+            break;
+          }
+        }
     }
 
     public static void mean() {
+        int number = s.nextInt();
+        int number1 = s.nextInt();
+while(true){
+    if(number >0 && number1 >0){
+System.out.print("The mean is " + (number + number1) /2);
+break;
+    }
+}
     }
 
     public static void main(String[] args) {
