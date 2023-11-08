e3387ad6511dcaeb34dd89d0c6384d78ac8f2bbc
MJFracess <mjfracess@gmail.com>
2023-11-08 15:00:07 -0800

    Completed Lab 007

--- src/ForLoops.java
+++ src/ForLoops.java
@@ -1,15 +1,67 @@
+/**
+ * @author T. Hartman
+ * @author MJ Fracess
+ * @since Version 1.0
+ */
+
 import java.util.Scanner;
 
 public class ForLoops {
     private final static Scanner s = new Scanner(System.in);
 
     public static void forSum() {
+        System.out.println("Enter a number to calculate if all the digits\n...starting at 1 through to the number you entered\nwere added together");
+        int n = s.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        System.out.printf("The sum is %d%n", sum );
     }
+        /* this was my first attempt and while I don't think it is wrong I think you were looking for something more simple.
+        I don't know why I tend to overcomplicate it at first.
+        while (true) {
+            if (s.hasNextInt()) {
+                int n = s.nextInt();
+
+                if (n>=1){
+                    int sum = 0;
+                    for (int i = 1; i <= n; i++){
+                        sum += i;
+                    }
+                    System.out.print("The sum is "+ sum);
+                } else {
+                    System.out.println("Please try again and enter valid number 1 and up");
+                }
+            } else {
+                System.out.println("Please try again and enter valid numbers 1 and up");
+                break;
+            }
+        */
+
 
     public static void rangeSum() {
+        System.out.println("Enter two numbers and then enter to calculate if all the digits from one to the other");
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int min = Math.min (a,b);
+        int max = Math.max (a,b);
+        int sum = 0;
+        for (int i = min; i <= max; i++) {
+            sum += i;
+        }
+        System.out.printf("The sum is %d%n", sum);
     }
 
     public static void factorial() {
+        System.out.println("Enter a number to calculate it's factorial");
+        int n = s.nextInt();
+        int f = 1;
+
+        for (int i = 1; i <= n; i++) {
+            f *= i;
+        }
+        System.out.print("The factorial is "+ f);
     }
 
     public static void main(String[] args) {
--- src/WhileLoops.java
+++ src/WhileLoops.java
@@ -1,16 +1,60 @@
+/**
+ * @author T. Hartman
+ * @author MJ Fracess
+ * @since Version 1.0
+ */
+
 import java.util.Scanner;
 
 public class WhileLoops {
     private final static Scanner s = new Scanner(System.in);
 
     public static void eoCount() {
+        int even = 0;       //Creates a count for even numbers
+        int odd = 0;        //Creates a count for odd numbers
+        do {
+            System.out.println("Enter a number");  //requesting a number from the user
+            int num = Integer.parseInt(s.nextLine());   //reads that number into a variable
+            if (num % 2 == 0) {         //if the number is even it will add 1 to the count
+                even++;
+            } else {                    //if the number is odd it will add 1 to the count
+                odd++;
+            }
+        }
+        while (even < 3 && odd < 3); //if 3 even or odd numbers are entered the loop will end
+
+        System.out.printf("%d Even numbers have been entered%n%d Odd number have been entered%n", even, odd);
     }
 
     public static void mean() {
+
+        double sum = 0;
+        int count = 0;
+
+        System.out.println("Enter a number to calculate the mean");  //requesting a number from the user
+        while (true) {
+            if (s.hasNextInt()) {
+                int num = s.nextInt();
+                sum += num;
+                count++;
+            } else {
+                System.out.println("Please try again and enter valid numbers 1 and up");
+                break;
+            }
+            if (count > 0) {
+                double mean = sum / count;
+                System.out.println("Mean of the entered numbers: " + mean);
+                System.out.println("Enter another number to continue calculating the mean of all numbers entered");
+            } else {
+                System.out.println("Please try again and enter valid number 1 and up");
+            }
+        }
     }
 
-    public static void main(String[] args) {
-        eoCount();
-        mean();
+    public static void main (String[]args){
+            eoCount();
+            mean();
+        }
     }
-}
+
+
