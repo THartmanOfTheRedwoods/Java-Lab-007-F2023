6930ca0c44672de9e58581418a100909b229bf7e
Lina <lina.guts12@gmail.com>
2023-12-10 23:54:08 -0800

    Completed Lab 007

--- src/ForLoops.java
+++ src/ForLoops.java
@@ -1,15 +1,49 @@
+/**
+ *
+ * @author Trevor Hartman
+ * @author Angelina Perez
+ *
+ * @since Version 1.0
+ *
+ */
 import java.util.Scanner;
 
 public class ForLoops {
     private final static Scanner s = new Scanner(System.in);
 
     public static void forSum() {
+        System.out.println("Number?");
+        int n = 0;
+        int in = s.nextInt();
+        int sum = 0;
+        for(int i=1; i <= n; i++) {
+            sum+= i;
+        }
+        System.out.println("The sum is" + sum);
     }
 
     public static void rangeSum() {
+        int one = s.nextInt();
+        System.out.println("Number? ");
+        int two = s.nextInt();
+        System.out.println("Number? ");
+        int sum = 0;
+        int start = Math.min(one, two);
+        int end = Math.max(one,two);
+        for(int i = start; i <= end; i++) {
+            sum+= i;
+        }
+        System.out.println("The sum is: " + sum);
     }
 
     public static void factorial() {
+        int number = s.nextInt();
+        System.out.println("Number:");
+        int factorial =1;
+        for(int i = 1; i <= number; i++) {
+            factorial *= i;
+        }
+        System.out.println("Factorial: " + factorial);
     }
 
     public static void main(String[] args) {
--- src/ReWrites.java
+++ src/ReWrites.java
@@ -1,3 +1,11 @@
+/**
+ *
+ * @author Trevor Hartman
+ * @author Angelina Perez
+ *
+ * @since Version 1.0
+ *
+ */
 import java.util.Scanner;
 
 class ReWrites {
@@ -8,7 +16,7 @@ class ReWrites {
         double sum = 0.0;
         int count = 0;
         System.out.println("Ths program will calculate the average of numbers entered. Begin entering and enter \"Q\" to quit.");
-        while(!(l = s.nextLine()).toUpperCase().equals("Q")) {
+        for (l = s.nextLine(); !l.toUpperCase().equals("Q"); l = s.nextLine()) {
             try {
                 sum += Integer.parseInt(l);
                 count++;
@@ -23,11 +31,8 @@ class ReWrites {
         int x = -1;
         int sum = 0;
 
-        while (true) {
+        while (x != 0) {
             try {
-                if(x == 0) {
-                    break;
-                }
                 x = Integer.parseInt(in.nextLine());
                 if (x <= 0) {
                     continue;
